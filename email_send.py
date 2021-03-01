0e6522521e35bc0cf2332ec72bdc0e310f76b1b4
xiaoliujinpeng <1240682960@qq.com>
2021-03-01 22:22:44 +0800

    完成基本功能

--- email_send.py
+++ email_send.py
@@ -5,14 +5,14 @@ from email.header import Header
 from email.utils import formataddr
 import os.path
 
-filedir = os.path.abspath(__file__)
 
-conf_file=os.path.join(filedir,"configure.json")
-
-configure = json.load(open(conf_file, "r"))
-receivers = ["1240682960@qq.com"]
 def send_email(target):
-    message = MIMEText("{},{},{}".format(target.get("title"),target.get("link"),target.get("time")), "plain", "utf-8")
+    filedir = os.path.dirname(os.path.abspath(__file__))
+    conf_file=os.path.join(filedir,"configure.json")
+    configure = json.load(open(conf_file, "r"))
+    receivers = ["1240682960@qq.com"]
+
+    message = MIMEText("{},{},{}".format(target.get('time'),target.get("title"),target.get("link")), "plain", "utf-8")
     message['From'] = formataddr(('我的爬虫',configure.get("MAIL_USER")))
     message['To'] = formataddr(('用户',receivers[0]))
 
